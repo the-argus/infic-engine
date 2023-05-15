c5954b55900206119e0b399ee99f31a0fb99d5ea
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-05-14 21:51:23 -0500

    build fixes

--- build.zig
+++ build.zig
@@ -1,7 +1,8 @@
 const raylib = @import("raylib/build.zig");
+const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-    const exe = ...;
+    const exe = "infic";
     raylib.addTo(b, exe, target);
 }
